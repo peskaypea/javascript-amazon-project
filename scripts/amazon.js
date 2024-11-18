3def086c3a26621332eb48e15442b6d9e7abc870
zane.reid <zane.reid@guestcentric.com>
2024-11-18 05:58:02 +0000

    load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,12 +1,14 @@
 import { cart, addToCart } from "../data/cart.js";
-import {products} from "../data/products.js";
+import { products, loadProducts } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
+loadProducts(renderProductsGrid);
 
-let productsHTML = "";
+function renderProductsGrid() {
+  let productsHTML = "";
 
-products.forEach((product) => {
-  productsHTML += `
+  products.forEach((product) => {
+    productsHTML += `
     <div class="product-container">
     <div class="product-image-container">
       <img class="product-image"
@@ -58,31 +60,25 @@ products.forEach((product) => {
       Add to Cart
     </button>
   </div>`;
-});
+  });
 
-document.querySelector(".js-products-grid").innerHTML =
-  productsHTML;
+  document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
-   function updateCartQuantity(){
-
+  function updateCartQuantity() {
     let cartQuantity = 0;
 
     cart.forEach((cartItem) => {
       cartQuantity += cartItem.quantity;
-    })
+    });
 
-    document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   }
 
-
-document.querySelectorAll(".js-add-to-cart").forEach((button) => {
-  button.addEventListener("click", () => {
-    const productId = button.dataset.productId;
-    addToCart(productId);
-    updateCartQuantity();
+  document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+    button.addEventListener("click", () => {
+      const productId = button.dataset.productId;
+      addToCart(productId);
+      updateCartQuantity();
+    });
   });
-
-
-
-});
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,10 @@
-import {renderOrderSummary} from './checkout/orderSummary.js';
-import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 // import '../data/cart-class.js';
-import '../data/backend-practise.js'
+// import '../data/backend-practise.js'
 
-renderOrderSummary();
-renderPaymentSummary();
-console.log(businessCart instanceof Cart)
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
